aa5349a0fd23c6bd9147d9703f879ddf3d5bb4fa
Jenna Allgeier <jenna.allgeier@gmail.com>
2022-03-29 17:02:41 -0400

    Button click on homepage is console logging

---
+++ client/src/components/AddPlant.jsx
@@ -0,0 +1,11 @@
+
+
+const AddPlant = () => {
+    return (
+        <div>
+            <p>Add plant!</p>
+        </div>
+    )
+}
+
+export default AddPlant
--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,11 +1,12 @@
 import logo from '../assets/LUSH.png';
+import AddPlant from './AddPlant'
 
 const Welcome = () => {
   return (
     <div>
         <img src={logo} className="app-logo" alt="logo" />
         <div>
-          <button className='add-plant-btn'>Add Your Plant!</button>
+          <button className="add-plant-btn" type="submit" onClick={() => console.log("I got clicked!") }>Add Your Plant!</button>
         </div>
     </div>
   )
